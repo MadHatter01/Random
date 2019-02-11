d4346f179a27e360ac187488765881588f63e64b
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2019-02-11 12:51:07 +0530

    Random Color Gen Methods

---
+++ Random Color Generator/color.js
@@ -0,0 +1,19 @@
+//Random Color Generator Method 1
+
+var red= Math.floor(Math.random() * 256);
+var green = Math.floor(Math.random() * 256);
+var blue = Math.floor(Math.random() * 256);
+var opacity = Math.random().toFixed(1);
+colorHex = "rgba("+red+","+green+","+blue+","+opacity+")"; 
+document.body.style.backgroundColor = colorHex;
+document.getElementById('parentColor').innerText=colorHex;
+
+//Random Color Generator Method 2
+
+var num=(Math.floor(Math.random() * (16777215-0) +0).toString(16));
+hex=(num.length < 6 ? '0':'')+num;
+
+var colorBox=document.getElementById('colorBox');
+console.log(hex);
+document.getElementById('colorLabel').innerText='#'+hex;
+colorBox.style.backgroundColor='#'+hex;
